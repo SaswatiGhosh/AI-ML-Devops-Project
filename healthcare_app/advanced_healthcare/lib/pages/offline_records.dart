20bebc16490c47760209ec25d3e139ae8f5e6517
4RN4B <arnabjuniormondal123@gmail.com>
2025-06-17 22:36:25 +0530

    updated upload-lab records page

--- healthcare_app/advanced_healthcare/lib/pages/offline_records.dart
+++ healthcare_app/advanced_healthcare/lib/pages/offline_records.dart
@@ -2,84 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:open_file/open_file.dart';
 
-
-class OfflineRecords extends StatelessWidget 
-{
+class OfflineRecords extends StatelessWidget {
   final File? file;
   final String? fileName;
 
-  const OfflineRecords({
-    super.key,
-    this.file,
-    this.fileName,
-  });
-  Future <Map<String, dynamic>> _loadSavedFile() async
-  {
-    final prefs=  await SharedPreferences.getInstance();
+  const OfflineRecords({super.key, this.file, this.fileName});
+  Future<Map<String, dynamic>> _loadSavedFile() async {
+    final prefs = await SharedPreferences.getInstance();
     final path = prefs.getString('offline_file_path');
-    final name= prefs.getString('offline_file_name');
+    final name = prefs.getString('offline_file_name');
 
     print("Loaded path: $path");
     print("Loaded name: $name");
 
-    if (path!= null && name!=null  && File(path).existsSync()){
-      return{'file' : File(path), 'fileName' : name};
-    }
-    else{
-      return {'file' : null, 'fileName' : null};
+    if (path != null && name != null && File(path).existsSync()) {
+      return {'file': File(path), 'fileName': name};
+    } else {
+      return {'file': null, 'fileName': null};
     }
   }
+
   @override
-Widget build(BuildContext context) 
-{
-  return FutureBuilder
-  (
-    future: file != null && fileName != null
-    ? Future.value({'file' : file, 'fileName' : fileName})
-    : _loadSavedFile(),
-    
-    
-    builder: (context,snapshot)
-    {
-      if (!snapshot.hasData) {
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: file != null && fileName != null
+          ? Future.value({'file': file, 'fileName': fileName})
+          : _loadSavedFile(),
+
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
           return const Scaffold(
             body: Center(child: CircularProgressIndicator()),
           );
         }
-      final file = snapshot.data!['file'] as File?;
-      final fileName = snapshot.data!['fileName'] as String?;
-    
-      final isFile= fileName != null &&(fileName.endsWith(".pdf") || fileName.endsWith(".docx") || fileName.endsWith("png"));
-        return Scaffold
-        (
+        final file = snapshot.data!['file'] as File?;
+        final fileName = snapshot.data!['fileName'] as String?;
 
-        appBar: AppBar(title: const Text("Offline Records")),
-        body: Center
-        (
-          child: file == null || fileName == null ? const Text("No file uploaded", style: TextStyle(fontSize: 18))
-          : Column
-          (
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("File Name: $fileName", style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 20),
-              isFile
-                ? const Icon(Icons.description, size: 100)  // 📄 icon for docx/pdf
-                : const Text("Unsupported file type"),
-              const SizedBox(height: 20),
-              ElevatedButton
-              (
-                onPressed: (){ (file != null) OpenFile.open(file.path)
-                },
-                child: const Text("Back"),
-              ),
-            ],
-            ),
+        final isFile =
+            fileName != null &&
+            (fileName.endsWith(".pdf") ||
+                fileName.endsWith(".docx") ||
+                fileName.endsWith("png"));
+        return Scaffold(
+          appBar: AppBar(title: const Text("Offline Records")),
+          body: Center(
+            child: file == null || fileName == null
+                ? const Text("No file uploaded", style: TextStyle(fontSize: 18))
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "File Name: $fileName",
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      const SizedBox(height: 20),
+                      isFile
+                          ? const Icon(
+                              Icons.description,
+                              size: 100,
+                            ) // 📄 icon for docx/pdf
+                          : const Text("Unsupported file type"),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (file != null) OpenFile.open(file.path);
+                        },
+                        child: const Text("Back"),
+                      ),
+                    ],
+                  ),
           ),
         );
       },
     );
-
   }
 }
--- healthcare_app/advanced_healthcare/lib/pages/upload_lab_records.dart
+++ healthcare_app/advanced_healthcare/lib/pages/upload_lab_records.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:advanced_healthcare/pages/offline_records.dart';
 
-
 class UploadLabReport extends StatefulWidget {
   const UploadLabReport({super.key});
   @override
@@ -15,19 +14,19 @@ class UploadLabReport extends StatefulWidget {
 
 class _UploadLabReportState extends State<UploadLabReport> {
   File? _selectedFile; // Stores the selected file
-  String? _fileName;   // Stores the name of the selected file
+  String? _fileName; // Stores the name of the selected file
 
   // Picks a file using the File Picker
   Future<void> _pickFile() async {
     final result = await FilePicker.platform.pickFiles();
 
     if (result != null && result.files.single.path != null) {
-      File file= File(result.files.single.path!);
-      String name= result.files.single.name;
+      File file = File(result.files.single.path!);
+      String name = result.files.single.name;
 
-      final prefs= await SharedPreferences.getInstance();
+      final prefs = await SharedPreferences.getInstance();
       await prefs.setString('offline_file_path', file.path);
-      await prefs.setString('offline_file_name' , name);
+      await prefs.setString('offline_file_name', name);
       setState(() {
         _selectedFile = file;
         _fileName = name;
@@ -35,84 +34,95 @@ class _UploadLabReportState extends State<UploadLabReport> {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => OfflineRecords(
-            file: file,
-            fileName:name ,
-          ),
+          builder: (context) => OfflineRecords(file: file, fileName: name),
         ),
       );
     }
   }
 
-Future<void> _uploadFile() async {
-  if (_selectedFile == null) return;
+  Future<void> _uploadFile() async {
+    if (_selectedFile == null) return;
 
-  final uri = Uri.parse('https://your-api-endpoint.com/upload');
+    final uri = Uri.parse('https://your-api-endpoint.com/upload');
 
-  // Create multipart request
-  var request = http.MultipartRequest('POST', uri)..files.add(await http.MultipartFile.fromPath('file', _selectedFile!.path));
+    // Create multipart request
+    var request = http.MultipartRequest('POST', uri)
+      ..files.add(
+        await http.MultipartFile.fromPath('file', _selectedFile!.path),
+      );
 
-  // Send the request
+    // Send the request
 
-  var response = await request.send();
-  if (response.statusCode == 200) {
-       ScaffoldMessenger.of(context).showSnackBar(
-         const SnackBar(content: Text('File uploaded successfully!')),
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('File uploaded successfully!')),
       );
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Failed to upload file.')),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Failed to upload file.')));
     }
   }
 
-
   @override
-  Widget build(BuildContext context) => 
-  Scaffold(appBar: AppBar(title: Text("Upload Lab Records"), backgroundColor: const Color.fromARGB(255, 218, 89, 241),), 
-  body: SafeArea(
-    child:Column(
-    
-    children:[
-      SizedBox(height:20),
-      Text("Upload your lab reports here", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
-      Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text("Upload Lab Records"),
+      backgroundColor: const Color.fromARGB(255, 218, 89, 241),
+    ),
+    body: SafeArea(
+      child: Column(
         children: [
-              
-              if (_fileName != null)
-                Text('Selected file: $_fileName'),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  onPressed: _pickFile,
-                  child: const Text('Pick File'),
+          SizedBox(height: 20),
+          Text(
+            "Upload your lab reports here",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (_fileName != null) Text('Selected file: $_fileName'),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: _pickFile,
+                        child: const Text('Pick File'),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _selectedFile != null ? _uploadFile: null,
-                child: const Text('Upload File'),
-              )
-        ]
+                // if (_fileName != null)
+                //   Text('Selected file: $_fileName'),
+                // const SizedBox(height: 20),
+                // Padding(
+                //   padding: const EdgeInsets.all(8.0),
+                //   child: ElevatedButton(
+                //     onPressed: _pickFile,
+                //     child: const Text('Pick File'),
+                //   ),
+                // ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _selectedFile != null ? _uploadFile : null,
+                      child: const Text('Upload File'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      )
-    ]
-  )
-  )
-);
+    ),
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
